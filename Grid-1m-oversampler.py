a20f5621ff1888825ebf0424303833aea8309978
cjaddison82 <82113275+cjaddison82@users.noreply.github.com>
2022-03-04 12:01:45 +0000

    Add files via upload
    python script in 2.7 to interate over a xyz dataset and reduce to 1m grid.

---
+++ Grid-1m-oversampler.py
@@ -0,0 +1,56 @@
+#INPUT FORMAT E N DEPTH
+#OUTPUT CSV AT METRE ONLY
+
+try:
+    from math import *
+except:
+    print "import error"
+
+T = int(1) #flag for while loop
+
+
+E = [] #new array to read from the RPL file
+N = []
+KP = []
+
+gridE = [] #new array to write logfile values to
+gridN = []
+gridZ = []
+
+
+
+
+
+    
+OUTPUT1 = open("D:/temp/OUTPUT2.TXT", "w")
+
+previous = str("0")
+
+
+
+fileGRID = open("D:/SHEFA_acceptedXYZ/XYZExportFanoe.xyz", "r") #filename of grid file being processed
+for line in fileGRID:
+    splitline = line.split(" ")#split on space because of supplied data, might be comma or semicolon in future
+
+    gridE = int(float((splitline[0])))
+    gridN = int(float((splitline[1])))
+    gridZ = float((splitline[2]))
+
+    checker = str(gridE)#+str(gridN)
+
+    if checker == previous:
+        pass
+    else:
+        OUTPUT1.write(str(gridE)+","+str(gridN)+","+str(gridZ)+"\n\r")
+        previous = str(gridE)#+str(gridN)
+        #print "new line written to file"
+
+    
+fileGRID.close()
+              
+
+OUTPUT1.close()
+
+
+
+        
